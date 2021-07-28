654263a0aeb48301b7474618e90691de75d45622
wesle <wesle@LAPTOP-EFB6LC9O>
2021-07-28 12:23:43 -0300

    Tela de login e criação de conta

--- src/app/page/login/login.component.ts
+++ src/app/page/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  login = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  loginStatus(status: any) {
+    this.login = status;
   }
 
 }
