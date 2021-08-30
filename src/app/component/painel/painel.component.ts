7f2fad22b127d8a1ee0df2ddbb8339f88fcbd423
wesle <wesle@LAPTOP-EFB6LC9O>
2021-08-30 16:21:28 -0300

    grafico

--- src/app/component/painel/painel.component.ts
+++ src/app/component/painel/painel.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FriendService } from 'src/app/service/friend.service';
+
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-painel',
@@ -7,7 +10,27 @@ import { FriendService } from 'src/app/service/friend.service';
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
+  @ViewChild("meuCanvas", { static: true }) elemento: ElementRef | undefined;
+
   userData: any;
+  friends: any;
+  qtdFriends: any = [];
+  loadingGraphic = false;
+
+  months: any = [
+    { id: 1, qtd: 0 },
+    { id: 2, qtd: 0 },
+    { id: 3, qtd: 0 },
+    { id: 4, qtd: 0 },
+    { id: 5, qtd: 0 },
+    { id: 6, qtd: 0 },
+    { id: 7, qtd: 0 },
+    { id: 8, qtd: 0 },
+    { id: 9, qtd: 0 },
+    { id: 10, qtd: 0 },
+    { id: 11, qtd: 0 },
+    { id: 12, qtd: 0 },
+  ];
 
   constructor(private friendService: FriendService) { }
 
@@ -15,15 +38,52 @@ export class PainelComponent implements OnInit {
     this.userData = JSON.parse(localStorage.getItem('usuario') || '{}');
 
     this.getAllFriends();
+
   }
 
   getAllFriends() {
+    this.loadingGraphic = true;
+
     const obj = {
       IdUser: this.userData.value.resUser.id
     };
 
     this.friendService.getAllFriends(obj).subscribe(res => {
-      // console.log('retorno da api ---> ', res);
+      this.loadingGraphic = false;
+      this.friends = res;
+
+      this.friends.value.resFriend.forEach((dayOfMonths: any) => {
+        this.months.forEach((element: any) => {
+          if (element.id == dayOfMonths.dtCreate.substring(5, 7)) {
+            element.qtd += 1;
+          }
+        });
+
+      });
+
+      this.months.forEach((qtd: any) => {
+        this.qtdFriends.push(qtd.qtd);
+      });
+
+      this.graphic();
+    });
+  }
+
+  graphic() {
+    new Chart(this.elemento?.nativeElement, {
+      type: 'line',
+      data: {
+        labels: ["jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"],
+        datasets: [
+          {
+            hoverBackgroundColor: '#000',
+            data: this.qtdFriends,
+            borderColor: '#ed145b',
+            backgroundColor: '#ed145b',
+            label: 'Amigos'
+          }
+        ]
+      }
     });
   }
 
