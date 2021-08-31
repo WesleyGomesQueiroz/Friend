d7605ddff0a81a6ee435b55e3a94cc5f78042683
wesle <wesle@LAPTOP-EFB6LC9O>
2021-08-31 13:31:28 -0300

    Lista de amigos

--- src/app/component/friend/friend.component.ts
+++ src/app/component/friend/friend.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FriendService } from 'src/app/service/friend.service';
 
 @Component({
   selector: 'app-friend',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./friend.component.css']
 })
 export class FriendComponent implements OnInit {
+  userData: any;
+  friends: any;
 
-  constructor() { }
+  constructor(public friendService: FriendService) { }
 
   ngOnInit(): void {
+    this.userData = JSON.parse(localStorage.getItem('usuario') || '{}');
+
+    this.getAllFriends();
+  }
+
+  getAllFriends() {
+    const obj = {
+      IdUser: this.userData.value.resUser.id
+    };
+
+    this.friendService.getAllFriends(obj).subscribe(res => {
+      this.friends = res;
+    });
+
   }
 
 }
